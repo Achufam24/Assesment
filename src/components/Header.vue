953d19c293b95f316e9fc044da8a326d2e168741
Achu-ulim Agbama <achuulimagbama@gmail.com>
2023-04-02 21:56:02 +0100

    update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
   <div class="container flex flex-wrap items-center justify-between mx-auto">
   <a href="/" class="flex items-center">
       <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo">
-      <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">BendSelect</span>
+      <span class="self-center text-xl font-semibold whitespace-nowrap">BendSelect</span>
   </a>
   <div class="flex md:order-2">
       <CartButtonVue/>
